81b67602ce66018eaf85dcfce545c1801d594ba6
clarabarretto <clarabarrettoestudo@gmail.com>
2023-03-27 10:32:28 -0300

    adicionando lista 4

---
+++ lista4/leao.py
@@ -0,0 +1,52 @@
+salarioSport = input().split()
+salarioNovo = input().split()
+
+def mergeSort(data):
+  tam_data = len(data)
+  if tam_data < 2:
+    return
+  meio = tam_data // 2 
+  
+  left_data = data[:meio]
+  right_data = data[meio:]
+
+  mergeSort(left_data)
+  mergeSort(right_data)
+
+  left_index = 0
+  right_index = 0
+  data_index = 0
+
+  while left_index < len(left_data) and right_index < len(right_data):
+    if int(left_data[left_index]) < int(right_data[right_index]):
+      data[data_index] = int(left_data[left_index])
+      left_index += 1
+    else:
+      data[data_index] = int(right_data[right_index])
+      right_index += 1
+    data_index += 1
+
+  while left_index < len(left_data):
+    data[data_index] = int(left_data[left_index])
+    left_index += 1
+    data_index += 1
+
+  while right_index < len(right_data):
+    data[data_index] = int(right_data[right_index])
+    right_index += 1
+    data_index += 1
+
+def getMediana(data):
+    if(len(data) % 2 == 0):
+      par = len(data) // 2
+      mediana = ((data[par-1] )+ data[par])/2
+      return mediana
+    else:
+      index = (len(data) / 2) + 0.5
+      mediana = data[int(index)]
+      return mediana
+
+arraySalarios = salarioNovo + salarioSport
+mergeSort(arraySalarios)
+mediana = getMediana(arraySalarios)
+print(f'O salário sugerido por Juba na primeira negociação será de {mediana:.2f} mil reais.')
---
+++ lista4/wolfam.py
@@ -0,0 +1,45 @@
+lista_folha = None
+def heapify(seq, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+    if l < n and seq[l] > seq[largest]:
+        largest = l
+    if r < n and seq[r] > seq[largest]:
+        largest = r
+    if largest != i:
+        seq[i], seq[largest] = seq[largest], seq[i]
+        heapify(seq, n, largest)
+
+def heapSort(seq):
+    global lista_folha
+    n = len(seq)
+    lista_folha = n // 2
+    for i in range(lista_folha - 1, -1, -1):
+        heapify(seq, n, i)
+
+def minimo(seq, f):
+  find = seq[f]
+  for i in range(f, len(seq)):
+    if seq[i] < find:
+      find = seq[i]
+  return find
+
+def jogo(seq, constante):
+    heapSort(seq)
+    rodadas = 0
+    while seq:
+        maior = seq[0]
+        k = maior - abs(minimo(seq, lista_folha) * constante)
+        if k > 0:
+            seq.extend([k])
+            heapSort(seq)
+        seq.remove(maior)
+        heapSort(seq)
+        rodadas += 1
+    return rodadas
+
+seq = list(map(int, input().split()))
+constante = int(input())
+rodadas = jogo(seq, constante)
+print(f"{rodadas} rodadas, partindo para a próxima!")
